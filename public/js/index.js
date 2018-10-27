65e70054d87d52001abd79c21e6f32d1a65f0d8a
Tommy Do <tommy.td89@gmail.com>
2018-10-27 19:30:18 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('Connected to server');
 
-	socket.emit('createMessage', {
-		from: 'Tommy',
-		text: 'Hello everyone'
-	});
 });
 
 socket.on('newMessage', function (message) {
