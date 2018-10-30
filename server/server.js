28f5a473393cde9e87060acdb9c212d158ab06df
Tommy Do <tommy.td89@gmail.com>
2018-10-30 18:58:25 -0400

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,7 +14,7 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
 	console.log('New user connected');
-
+   
 	socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
 	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
@@ -24,11 +24,10 @@ io.on('connection', (socket) => {
 		console.log('Create Message', message)
 		io.emit('newMessage', generateMessage(message.from, message.text));
 		callback('This is from the server.');
-		// socket.broadcast.emit('newMessage', {
-		// 	from: message.from,
-		// 	text: message.text,
-		// 	createdAt: new Date().getTime()
-		// });
+	});
+
+	socket.on('createLocationMessage', (coords) => {
+		io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
 	});
 
 	socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
 	it('should generate correct message object', () => {
@@ -9,5 +9,19 @@ describe('generateMessage', () => {
 		var message = generateMessage(from, text);
 		expect(message).toInclude({from, text})
 		expect(typeof message.createdAt).toBe('number');
+	});
+});
+
+describe('generateLocationMessage', () => {
+	it('should generate correct location object', () => {
+		var from = 'Tommy';
+		var latitude = 1;
+		var longitude = 1;
+		var url = 'https://www.google.com/maps?q=1,1';
+		var locationMessage = generateLocationMessage(from, latitude, longitude);
+		expect(locationMessage).toInclude({
+			from,
+			url
+		});
 	});
 });
